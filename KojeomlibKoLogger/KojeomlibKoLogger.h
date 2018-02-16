a35d2e3018ccb4577341423041e54655058a1e35
kuma_jjw <kojeomstudio@gmail.com>
2018-02-16 23:24:02 +0900

    # boost lib 추가 및 logger 클래스 코드 일부 수정.
    1. 로그 메세지를 formatting 하여 저장.

--- KojeomlibKoLogger/KojeomlibKoLogger.h
+++ KojeomlibKoLogger/KojeomlibKoLogger.h
@@ -3,19 +3,37 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <time.h>
+#include <boost\format.hpp>
+#ifdef KOJEOMLIB_LOGGER_EXPORTS  
+#define KOJEOMLIB_LOGGER_API __declspec(dllexport)   
+#else  
+#define KOJEOMLIB_LOGGER_API __declspec(dllimport)   
+#endif 
+
+//http://www.cplusplus.com/doc/tutorial/files/
+
 namespace KojeomlibLogger {
 	enum LOG_TYPE {
-		TYPE_NORMAL = 0,
+		TYPE_WARNNING = 0,
 		TYPE_ERROR = 1
 	};
 	class KoLogger {
 	public:
+		KoLogger();
+		~KoLogger();
 		void SetMsgToLogFileBuffer(std::string logMsg, LOG_TYPE type);
 		void FlushLogBufferToFile();
+		// 로그파일이 저장될 파일 path ( 파일이름 포함.)
+		void SetLogFilePath(std::string pathWithFileName);
 	private:
 		std::ifstream ifstreamObj;
 		std::ofstream ofstreamObj;
+		std::string CreateLogMessage(LOG_TYPE type, std::string msg);
 		std::vector<std::string> logFileBuffer;
+		std::string logFilePath;
+		time_t systemTimer;
+		tm timeInfo;
 	};
 }
 
--- KojeomlibKoLogger/testmain.cpp
+++ KojeomlibKoLogger/testmain.cpp
@@ -1,5 +1,10 @@
 
 #include "stdafx.h"
+#include "KojeomlibKoLogger.h"
 void main() {
-
+	KojeomlibLogger::KoLogger logger;
+	logger.SetLogFilePath("D:\\test.txt");
+	logger.SetMsgToLogFileBuffer("test_00", KojeomlibLogger::LOG_TYPE::TYPE_ERROR);
+	logger.SetMsgToLogFileBuffer("test_44", KojeomlibLogger::LOG_TYPE::TYPE_ERROR);
+	logger.FlushLogBufferToFile();
 }
