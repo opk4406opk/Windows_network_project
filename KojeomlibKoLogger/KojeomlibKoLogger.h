382c604437076751212da7ff51982756bce21e0b
kuma_jjw <kojeomstudio@gmail.com>
2018-02-17 01:39:58 +0900

    # 버그 수정 및 불필요한 코드 정리

--- KojeomlibKoLogger/KojeomlibKoLogger.h
+++ KojeomlibKoLogger/KojeomlibKoLogger.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <time.h>
+#include <stdlib.h>
 #include <boost\format.hpp>
 #ifdef KOJEOMLIB_LOGGER_EXPORTS  
 #define KOJEOMLIB_LOGGER_API __declspec(dllexport)   
@@ -12,6 +13,8 @@
 #endif 
 
 //http://www.cplusplus.com/doc/tutorial/files/
+
+#define LOG_FILE_EXTENT ".txt"
 
 namespace KojeomlibLogger {
 	enum LOG_TYPE {
@@ -24,14 +27,17 @@ namespace KojeomlibLogger {
 		~KoLogger();
 		void SetMsgToLogFileBuffer(std::string logMsg, LOG_TYPE type);
 		void FlushLogBufferToFile();
-		// 로그파일이 저장될 파일 path ( 파일이름 포함.)
-		void SetLogFilePath(std::string pathWithFileName);
+		// 로그파일이 저장될 디렉토리 path.
+		void SetDirectory(std::string dirPath);
 	private:
 		std::ifstream ifstreamObj;
 		std::ofstream ofstreamObj;
 		std::string CreateLogMessage(LOG_TYPE type, std::string msg);
+		// 로컬머신을 기준으로 타임스탬프를 가져옵니다.
+		std::string GetTimeStamp();
 		std::vector<std::string> logFileBuffer;
-		std::string logFilePath;
+		// 로그파일이 저장될 디렉토리 path.
+		std::string logFileDirPath;
 		time_t systemTimer;
 		tm timeInfo;
 	};
--- KojeomlibKoLogger/testmain.cpp
+++ KojeomlibKoLogger/testmain.cpp
@@ -3,7 +3,7 @@
 #include "KojeomlibKoLogger.h"
 void main() {
 	KojeomlibLogger::KoLogger logger;
-	logger.SetLogFilePath("D:\\test.txt");
+	logger.SetDirectory("D:\\");
 	logger.SetMsgToLogFileBuffer("test_00", KojeomlibLogger::LOG_TYPE::TYPE_ERROR);
 	logger.SetMsgToLogFileBuffer("test_44", KojeomlibLogger::LOG_TYPE::TYPE_ERROR);
 	logger.FlushLogBufferToFile();
