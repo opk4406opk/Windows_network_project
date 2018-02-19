3870b6bfe7bfefc4e401a3228892a8269864280b
kuma_jjw <kojeomstudio@gmail.com>
2018-02-19 23:29:16 +0900

    # logger-> 스레드 safe한 코드 추가 / logger 클래스 해제 부분 코드 수정.
     1. 스레드 sfae한 코드 추가
     -> 기존 메소드에 lock_guard를 이용한 block-scoped locking.
     2. logger 클래스 파괴자 코드 수정.

--- KojeomlibKoLogger/KojeomlibKoLogger.h
+++ KojeomlibKoLogger/KojeomlibKoLogger.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <time.h>
+#include <mutex>
 #include <boost\format.hpp>
 #ifdef KOJEOMLIB_LOGGER_EXPORTS  
 #define KOJEOMLIB_LOGGER_API __declspec(dllexport)   
@@ -24,7 +25,15 @@ namespace KojeomlibLogger {
 	public:
 		KoLogger();
 		~KoLogger();
+		// 메세지 로그를 Console 창으로 출력합니다. ( Not thread safe. )
+		void MsgGoToConsole(std::string logMsg, LOG_TYPE type);
+		//메세지 로그를 Console 창으로 출력합니다. ( Thread safe. )
+		void MsgGoToConsoleSafely(std::string logMsg, LOG_TYPE type);
+		// 메세지 로그를 파일 버퍼에 저장한다. ( Not thread safe.)
 		void SetMsgToLogFileBuffer(std::string logMsg, LOG_TYPE type);
+		// 메세지 로그를 파일 버퍼에 저장한다. ( Thread safe. )
+		void SetMsgToLogFileBufferSafely(std::string logMsg, LOG_TYPE type);
+		// 파일 버퍼를 외부 파일로 저장합니다. ( Not thread safe. )
 		void FlushLogBufferToFile();
 		// 로그파일이 저장될 디렉토리 path.
 		void SetDirectory(std::string dirPath);
@@ -37,6 +46,8 @@ namespace KojeomlibLogger {
 		std::vector<std::string> logFileBuffer;
 		// 로그파일이 저장될 디렉토리 path.
 		std::string logFileDirPath;
+		// 스레드 동기화에 쓰이는 mutex.
+		std::mutex mutexObj;
 	};
 }
 
